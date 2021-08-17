f32b7a76f93226dd22b6c7f00b72e9ac4e152aa2
Burhan Uddin <whoisburhan@gmail.com>
2021-08-17 15:39:16 +0600

    update

--- Assets/Scripts/SoundButtonScript.cs
+++ Assets/Scripts/SoundButtonScript.cs
@@ -5,16 +5,40 @@ using UnityEngine.UI;
 
 namespace GS.TilesMatch
 {
+    [RequireComponent(typeof(Button))]
     public class SoundButtonScript : MonoBehaviour
     {
         [Header("Color")]
         [SerializeField] private Color muteCOlor;
         [SerializeField] private Color unMuteColor;
 
-        [Header("Transform")]
-        [SerializeField] private Transform circle;
+        [Header("RectTransform")]
+        [SerializeField] private RectTransform circle;
 
         [Header("Image")]
         [SerializeField] private Image ButtonUIImg;
+
+        private Button button;
+
+        private void Awake()
+        {
+            button = GetComponent<Button>();
+        }
+
+        private void Start()
+        {
+            button.onClick.AddListener(() => 
+            {
+                if(circle.position.x > 0f)
+                {
+
+                }
+            });
+        }
+
+        private void Animate()
+        {
+
+        }
     }
 }
